63d38bc2e12fec373d0c29e2e65dcdf51d67933a
jayking <mojunseo49@naver.com>
2024-10-29 15:06:50 +0900

    add: init

---
+++ src/main/kotlin/org/example/springbatchtutorial/SpringbatchTutorialApplication.kt
@@ -0,0 +1,11 @@
+package org.example.springbatchtutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbatchTutorialApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbatchTutorialApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/springbatchtutorial/SpringbatchTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.springbatchtutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbatchTutorialApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
